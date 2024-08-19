220aaf183fabc877340b00b78808e356b4e121ad
SUYAB123 <111383970+SUYAB123@users.noreply.github.com>
2024-08-19 22:08:35 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,17 @@
 package com.driver;
+public class A {
+    public String meth() {
+        return "Invoking method from class A";
+    }
+}
+public class B extends A {
+    @Override
+    public String meth() {
+        return "Method is overridden in Extended class B";
+    }
+}
 
 public class Main {
-  
+      B objB = new B();
+        System.out.println(objB.meth()); 
 }
